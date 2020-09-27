35677369a2b733be05a34d90a6a4f8c82e6e0c73
Asalmerontkd <psrantonio@gmail.com>
2020-09-27 18:35:59 -0500

    Diseno settings

--- lib/src/pages/settings_page.dart
+++ lib/src/pages/settings_page.dart
@@ -1,7 +1,27 @@
 import 'package:flutter/material.dart';
+import 'package:preferenciasusuarioapp/src/widgets/menu_widget.dart';
 
-class SettingsPage extends StatelessWidget {
+class SettingsPage extends StatefulWidget {
   static final String routeName = 'settings';
+
+  @override
+  _SettingsPageState createState() => _SettingsPageState();
+}
+
+class _SettingsPageState extends State<SettingsPage> {
+  bool _colorSecundario = false;
+  int _genero = 1;
+  String _nombre = 'Pedro';
+
+  TextEditingController _textController;
+
+  @override
+  void initState() { 
+    super.initState();
+    _textController = new TextEditingController(
+      text: _nombre
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -9,9 +29,57 @@ class SettingsPage extends StatelessWidget {
       appBar: AppBar(
         title: Text('Ajustes'),
       ),
-      body: Center(
-        child: Text('Ajustes Page'),
-      )
+      body: ListView(
+        children: <Widget> [
+          Container(
+            padding: EdgeInsets.all(5.0),
+            child: Text('Settings', style: TextStyle(fontSize: 45.0, fontWeight: FontWeight.bold),),
+          ),
+          Divider(),
+          SwitchListTile(
+            value: _colorSecundario, 
+            title: Text('Color Secundario'),
+            onChanged: (value){
+              setState(() {
+                _colorSecundario = value;
+              });
+            }
+          ),
+          RadioListTile(
+            value: 1, 
+            groupValue: _genero, 
+            title: Text('Masculino'),
+            onChanged: (value){
+              setState(() {
+                _genero = value;
+              });
+            }
+          ),
+          RadioListTile(
+            value: 2, 
+            groupValue: _genero, 
+            title: Text('Femenino'),
+            onChanged: (value){
+              setState(() {
+                _genero = value;
+              });
+            }
+          ),
+          Divider(),
+          Container(
+            padding: EdgeInsets.symmetric(horizontal: 20.0),
+            child: TextField(
+              controller: _textController,
+              decoration: InputDecoration(
+                labelText: 'Nombre',
+                helperText: 'Nombre de la persona usando el telefono'
+              ),
+              onChanged: (value){},
+            ),
+          )
+        ],
+      ) ,
+      drawer: MenuWidget(),
     );
   }
 }
